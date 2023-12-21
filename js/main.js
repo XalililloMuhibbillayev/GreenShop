88ad855fae210905ab71d583f216bcd3311d8d8f
XalililloMuhibbillayev <135448124+XalililloMuhibbillayev@users.noreply.github.com>
2023-12-21 14:29:35 +0500

    HomePage

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 const body = document.querySelector("body");
-const overlay = document.querySelector("#overlay");
 const nav_button = document.querySelector(".nav-button");
 const xmark = document.querySelector(".close-icon");
 
 nav_button.addEventListener("click", () => body.classList.add("active"))
 xmark.addEventListener("click", () => body.classList.remove("active"))
-    
